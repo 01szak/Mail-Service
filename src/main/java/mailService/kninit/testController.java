5898893e708db41a23c1a0dd0958f80153372cb1
01szak <kacperolszewski2006@gmail.com>
2025-04-01 16:27:04 +0200

    initial setup

---
+++ src/main/java/mailService/kninit/testController.java
@@ -0,0 +1,15 @@
+package mailService.kninit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class testController {
+
+    @GetMapping("/test")
+    public String test (){
+        return "Hello World";
+    }
+}
